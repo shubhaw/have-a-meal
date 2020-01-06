53aa897fe3212039bd86c51aa44044a212910c95
Shubhaw Kumar <dev.shubhaw@gmail.com>
2020-01-06 13:56:36 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant="h1">Have a Meal</Typography>
+    </div>
+  );
+}
+
+export default App;
