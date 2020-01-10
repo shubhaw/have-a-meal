7f349dc207e134c643fb3a55caeb379f1deb16b2
Shubhaw Kumar <dev.shubhaw@gmail.com>
2020-01-11 00:35:44 +0530

    configuring axios and fetching restaurant list from cloud

--- src/components/Restaurants/Restaurants.js
+++ src/components/Restaurants/Restaurants.js
@@ -1,31 +1,52 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Restaurant from './Restaurant';
+import axios from '../../axios-config';
+
 import burgerImage from '../../assets/images/burger.jpg';
 
 const Restaurants = () => {
-    const restaurantList = [
-        {
-            name: 'Burfi Ghar',
-            description: 'Burfi Ghar serves delicious sweets and street food.',
-            image: burgerImage,
-            priceForTwo: '₹200 for 2',
-            rating: 5
-        },
-        {
-            name: 'Olive Bistro',
-            description: 'Best authentic Mexican food with lake side view!',
-            image: burgerImage,
-            priceForTwo: '₹1200 for 2',
-            rating: 3
-        },
-        {
-            name: 'Wich Please',
-            description: 'Serves delicious sandwiches and wraps with cheesy dips.',
-            image: burgerImage,
-            priceForTwo: '₹500 for 2',
-            rating: 4
-        }
-    ];
+    //#region hardcoded restaurant list
+    // const restaurantList = [
+    //     {
+    //         name: 'Burfi Ghar',
+    //         description: 'Burfi Ghar serves delicious sweets and street food.',
+    //         image: burgerImage,
+    //         priceForTwo: '₹200 for 2',
+    //         rating: 5
+    //     },
+    //     {
+    //         name: 'Olive Bistro',
+    //         description: 'Best authentic Mexican food with lake side view!',
+    //         image: burgerImage,
+    //         priceForTwo: '₹1200 for 2',
+    //         rating: 3
+    //     },
+    //     {
+    //         name: 'Wich Please',
+    //         description: 'Serves delicious sandwiches and wraps with cheesy dips.',
+    //         image: burgerImage,
+    //         priceForTwo: '₹500 for 2',
+    //         rating: 4
+    //     }
+    // ];
+    //#endregion
+
+    const [restaurantList, setRestaurantList] = useState([]);
+
+    useEffect(() => {
+        axios.get('getRestaurants')
+            .then(response => {
+                // console.log(response)
+                const restaurants = response.data.map(restaurant => {
+                    return {
+                        ...restaurant,
+                        image: burgerImage
+                    }
+                })
+                setRestaurantList(restaurants);
+            })
+            .catch(err => console.error(err));
+    }, [])
 
     return (
         <React.Fragment>
